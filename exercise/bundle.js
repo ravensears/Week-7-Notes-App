e706e10483df9ea0b5859d775298247a5819c174
Raven Sears <ravenrosereynolds@gmail.com>
2022-01-31 16:54:07 +0000

    Working with Bundle and Making files load in the HTML page

--- exercise/bundle.js
+++ exercise/bundle.js
@@ -1,5 +1,31 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // add.js
+  var require_add = __commonJS({
+    "add.js"(exports, module) {
+      module.exports = (a, b) => {
+        return a + b;
+      };
+    }
+  });
+
+  // multiply.js
+  var require_multiply = __commonJS({
+    "multiply.js"(exports, module) {
+      module.exports = (a, b) => {
+        return a * b;
+      };
+    }
+  });
+
   // index.js
+  var add = require_add();
+  var multiply = require_multiply();
+  console.log(multiply(add(4, 6), 7));
   console.log("Hello from the developer console!");
   console.log(new Date());
 })();
--- exercise/index.js
+++ exercise/index.js
@@ -1,10 +1,8 @@
-// console.log doesn't print anything on the page
-// it is not meant to be visible to the user, but for you
-// to help in debugging and getting visibility in your JS code.
-//
-// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+const add = require('./add');
+const multiply = require('./multiply');
+
+
+console.log(multiply(add(4, 6), 7));
 
 console.log('Hello from the developer console!');
-
 console.log(new Date());
-
