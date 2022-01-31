4e87ce6da4555c09b3d8238b07d88954d7bab144
Raven Sears <ravenrosereynolds@gmail.com>
2022-01-31 15:35:24 +0000

    Counter

---
+++ simpleCounter/counter-app/counterView.js
@@ -0,0 +1,25 @@
+class CounterView {
+  constructor(model) {
+    this.model = model;
+
+    const incrementButtonEl = document.querySelector('#increment-btn');
+    const decrementButtonEl = document.querySelector('#decrement-btn');
+    
+    incrementButtonEl.addEventListener('click', () => {
+      // Increment on the model, then refresh the view
+      this.model.increment();
+      this.display();
+    });
+
+    decrementButtonEl.addEventListener('click', () => {
+      this.model.decrement();
+      this.display();
+    });
+  }
+
+  display() {
+    document.querySelector('#counter').innerText = this.model.getCounter();
+  }
+}
+
+module.exports = CounterView;
